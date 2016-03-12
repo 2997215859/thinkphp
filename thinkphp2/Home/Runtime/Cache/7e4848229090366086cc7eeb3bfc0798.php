8e41cf0815ca9eb238728953ca842db4fb2d0cf9
2997215859 <2997215859@qq.com>
2016-03-12 20:53:27 +0800

    更新thinkphp2和thinkphp
    更新thinkphp2和thinkphp

---
+++ thinkphp2/Home/Runtime/Cache/7e4848229090366086cc7eeb3bfc0798.php
@@ -0,0 +1,14 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<p>我是header</p>
+
+<body>
+	<p>我是渲染页面</p>
+	
+<p>index</p>
+</body>
+</html>
